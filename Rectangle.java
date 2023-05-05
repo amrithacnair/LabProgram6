b3fbb4e8c55e7bb66e4d1cccea051a46ed618978
amrithacnair <127940305+amrithacnair@users.noreply.github.com>
2023-05-05 12:50:49 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,34 @@
+class tt
+ {
+    int lenght;
+    int breath;
+
+    tt(int l, int b)
+    {
+        this.lenght = l;
+        this.breath = b;
+    }
+
+    public int area()
+    {
+        return lenght * breath;
+    }
+}
+ public class Rectangle
+{
+    public static void main(String[] args) 
+{
+        tt obj = new tt(4,5);
+        tt obj1 = new tt(5,8);
+
+        System.out.println(obj.area());
+        System.out.println(obj1.area());
+
+
+
+
+
+ }
+
+
+}
